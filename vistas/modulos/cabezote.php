326278b749954520244e187287fbe96158c8fadc
Gerardo Muñoz <gerardomx17@gmail.com>
2021-02-18 23:09:59 -0600

    Maquetando el Cabezote con HTML5

---
+++ vistas/modulos/cabezote.php
@@ -0,0 +1,40 @@
+<!-- =========== TOP ========== -->
+<div class="container-fluid barraSuperior" id="top">
+    <div class="container">
+        <div class="row">
+            <!-- SOCIAL -->
+            <div class="col-lg-9 col-md-9 col-sm-8 col-12 social">
+                <ul>
+                    <li>
+                        <a href="http://facebook.com/" target="_blank">
+                            <i class="bi bi-facebook redSocial facebookBlanco"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="http://instagram.com/" target="_blank">
+                            <i class="bi bi-instagram redSocial instagramBlanco"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="http://twitter.com/" target="_blank">
+                            <i class="bi bi-twitter redSocial twitterBlanco"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="http://youtube.com/" target="_blank">
+                            <i class="bi bi-youtube redSocial youtubeBlanco"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <!-- INICIO-SESION/REGISTRO -->
+            <div class="col-lg-3 col-md-3 col-sm-4 col-12 registro">
+                <ul>
+                    <li><a href="#modalIngreso" data-toggle="modal">Ingresar</a></li>
+                    <li>|</li>
+                    <li><a href="#modalRgistro" data-toggle="modal">Crear una cuenta</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -4,14 +4,24 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="title" content="Tienda Virtual">
+    <meta name="description" content="Una descripción del sitio">
+    <meta name="keyword" content="palabras, claves, del, sitio">  
+    
+    <title>Ecommerce-Curso</title>
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
 
-    <title>Hola, Mundo</title>
+    
   </head>
   <body>
-    <h1>Hola Mundo!</h1>
+
+    <?php
+    //CABEZOTE
+    include "modulos/cabezote.php";
+    ?>
 
     <!-- Optional JavaScript; choose one of the two! -->
 
