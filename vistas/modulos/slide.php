1b81a5091dcb8197670508eef6d02217ba4ad747
Gerardo Muñoz <gerardomx17@gmail.com>
2021-04-02 00:54:54 -0600

    Maquetando el Slide - parte 2

--- vistas/modulos/slide.php
+++ vistas/modulos/slide.php
@@ -8,11 +8,62 @@
                 <li>
                     <img src="http://localhost/cursophp/backend/vistas/img/slide/default/back_default.jpg" alt="">
                     <div class="slideOpciones slideOpcion1">
-                        <img src="http://localhost/cursophp/backend/vistas/img/slide/slide1/calzado.png" alt="">
-                        <div class="textosSlide">
-                            <h1>Lorem Ipsum</h1>
-                            <h2>Lorem ipsum dolor sit amet</h2>
-                            <h3>Lorem ipsum dolor sit amet</h3>
+                        <img src="http://localhost/cursophp/backend/vistas/img/slide/slide1/calzado.png" alt="" style="top:10%; right:5%; width:35%">
+                        <div class="textosSlide" style="top:25%; left:10%; width:40%">
+                            <h1 style="color:#333">Lorem Ipsum</h1>
+                            <h2 style="color:#888">Lorem ipsum dolor sit amet</h2>
+                            <h3 style="color:#888">Lorem ipsum dolor sit amet</h3>
+                            <a href="#">
+                                <button class="btn btn-default backColor text-uppercase">
+                                Ver producto <i class="bi bi-chevron-compact-right"></i>
+                                </button>
+                            </a>
+                        </div>
+                    </div>
+                </li>
+                <!-- SLIDE 2 -->
+                <li>
+                    <img src="http://localhost/cursophp/backend/vistas/img/slide/default/back_default.jpg" alt="">
+                    <div class="slideOpciones slideOpcion2">
+                        <img src="http://localhost/cursophp/backend/vistas/img/slide/slide2/curso.png" alt="" style="top:5%; left:15%; width:25%">
+                        <div class="textosSlide" style="top:20%; right:10%; width:40%">
+                            <h1 style="color:#333">Lorem Ipsum</h1>
+                            <h2 style="color:#888">Lorem ipsum dolor sit amet</h2>
+                            <h3 style="color:#888">Lorem ipsum dolor sit amet</h3>
+                            <a href="#">
+                                <button class="btn btn-default backColor text-uppercase">
+                                Ver producto <i class="bi bi-chevron-compact-right"></i>
+                                </button>
+                            </a>
+                        </div>
+                    </div>
+                </li>
+                <!-- SLIDE 3 -->
+                <li>
+                    <img src="http://localhost/cursophp/backend/vistas/img/slide/slide3/fondo2.jpg" alt="">
+                    <div class="slideOpciones slideOpcion2">
+                        <img src="http://localhost/cursophp/backend/vistas/img/slide/slide3/iphone.png" alt="" style="top:5%; left:15%; width:25%">
+                        <div class="textosSlide" style="top:20%; right:10%; width:40%">
+                            <h1 style="color:#fff">Lorem Ipsum</h1>
+                            <h2 style="color:#888">Lorem ipsum dolor sit amet</h2>
+                            <h3 style="color:#888">Lorem ipsum dolor sit amet</h3>
+                            <a href="#">
+                                <button class="btn btn-default backColor text-uppercase">
+                                Ver producto <i class="bi bi-chevron-compact-right"></i>
+                                </button>
+                            </a>
+                        </div>
+                    </div>
+                </li>
+                <!-- SLIDE 4 -->
+                <li>
+                    <img src="http://localhost/cursophp/backend/vistas/img/slide/slide4/fondo3.jpg" alt="">
+                    <div class="slideOpciones slideOpcion1">
+                        <img src="" alt="" style="top:5%; right:15%; width:25%">
+                        <div class="textosSlide" style="top:20%; left:10%; width:40%">
+                            <h1 style="color:#fff">Lorem Ipsum</h1>
+                            <h2 style="color:#888">Lorem ipsum dolor sit amet</h2>
+                            <h3 style="color:#888">Lorem ipsum dolor sit amet</h3>
                             <a href="#">
                                 <button class="btn btn-default backColor text-uppercase">
                                 Ver producto <i class="bi bi-chevron-compact-right"></i>
@@ -22,6 +73,16 @@
                     </div>
                 </li>
             </ul>
+            <!-- ========= PAGINACIÓN ========== -->
+            <ol id="paginacion">
+                <li item="1"><span class="bi bi-circle"></span></li>
+                <li item="2"><span class="bi bi-circle"></span></li>
+                <li item="3"><span class="bi bi-circle"></span></li>
+                <li item="4"><span class="bi bi-circle"></span></li>
+            </ol>
+            <!-- ========= FLECHAS ========= -->
+            <div class="flechas" id="retroceder"><span class="bi bi-chevron-compact-left"></span></div>
+            <div class="flechas" id="avanzar"><span class="bi bi-chevron-compact-right"></span></div>
         </div>
     </div>
 </div>
